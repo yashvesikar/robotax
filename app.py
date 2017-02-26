f326f94373b5a48dc302a6329099053150c557f5
yashvesikar <yashvesikar@gmail.com>
2017-02-26 06:17:49 -0600

    .

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
+
     # tax_rate = 0.1
     #
     # #Employee data
-    # total_salary = sum(emp.Salary() for emp in array_of_employees if emp.Status() == False)
+    # total_tax = sum(emp.Tax() for emp in array_of_employees if emp.Status() == False)
     # total_production = sum(emp.Production() for emp in array_of_employees if emp.Status() == False)
     #
     # #Left hand side of equation, employee side
-    # lhs = (total_salary * tax_rate) / total_production
+    # lhs = (total_tax) / total_production
     #
     # #Robot data
     # robots_added = len(array_of_automations)
@@ -33,9 +34,18 @@ def main():
 
 @app.route('/calculations',methods=['POST'])
 def calculations():
-    print(request.form.to_dict().keys()[0])
+    print(request.form.to_dict())
 
-
+    # array_of_employees = []
+    # array_of_automations = []
+    #
+    # for employee in request.form.getlist('oldEmployees[]'):
+    #     salary = employee[2]['value']
+    #     productivity = employee[3]['value']
+    #     status = False
+    #     array_of_employees.append(Employee(salary,productivity,status))
+    # for item in array_of_employees:
+    #     print(item)
     return render_template('index.html')
     # read the posted values from the UI
     # _name = request.form['inputName']
