19762bac84cb133168b4409180dcdd9e165447a7
Bogdan Pozderca <bogdanpozderca@gmail.com>
2017-02-26 04:24:25 -0600

    :sparkles: front end submits to back end

--- app.py
+++ app.py
@@ -1,37 +1,40 @@
 from classes.automation import Automation
 from classes.employee import Employee
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    tax_rate = 0.1
+    # tax_rate = 0.1
+    #
+    # #Employee data
+    # total_salary = sum(emp.Salary() for emp in array_of_employees if emp.Status() == False)
+    # total_production = sum(emp.Production() for emp in array_of_employees if emp.Status() == False)
+    #
+    # #Left hand side of equation, employee side
+    # lhs = (total_salary * tax_rate) / total_production
+    #
+    # #Robot data
+    # robots_added = len(array_of_automations)
+    # robots_production = sum(automation.Production() for automation in array_of_automations)
+    # #Technician data/new employee data
+    # technicians_tax = sum(emp.Salary() for emp in array_of_employees if emp.Status() == True)* tax_rate
+    #
+    #
+    # robot_tax = (lhs*robots_production) - technicians_tax
+    #
+    # print("Robot Tax: ", robot_tax)
 
-    #Employee data
-    total_salary = sum(emp.Salary() for emp in array_of_employees if emp.Status() == False)
-    total_production = sum(emp.Production() for emp in array_of_employees if emp.Status() == False)
-
-    #Left hand side of equation, employee side
-    lhs = (total_salary * tax_rate) / total_production
-
-    #Robot data
-    robots_added = len(array_of_automations)
-    robots_production = sum(automation.Production() for automation in array_of_automations)
-    #Technician data/new employee data
-    technicians_tax = sum(emp.Salary() for emp in array_of_employees if emp.Status() == True)* tax_rate
-
-
-    robot_tax = (lhs*robots_production) - technicians_tax
-
-    print("Robot Tax: ", robot_tax)
-
-    print("Per robot tax: ", robot_tax/robots_added)
+    # print("Per robot tax: ", robot_tax/robots_added)
 
     return render_template('index.html')
 
-#@app.route('/',methods=['POST'])
-#def main():
+@app.route('/calculations',methods=['POST'])
+def calculations():
+    print(request.form);
+
+    return render_template('index.html')
     # read the posted values from the UI
     # _name = request.form['inputName']
     # _email = request.form['inputEmail']
--- static/javascripts/index.js
+++ static/javascripts/index.js
@@ -1,5 +1,9 @@
 var stepTwo = false
+var stepThree = false
+var stepSubmit = false
 var formStr = '<form><input type="text" name="numberOfEmployees" id="num-emp" class="" placeholder="# Employee" required><input type="text" name="averageTax" id="avg-tax" class="" placeholder="Average Tax" required><input type="text" name="wage" id="wage" class="" placeholder="Wage" required><input type="text" name="productivity" id="num-emp" class="" placeholder="Productivity" required></form>'
+var step2FormStr = '<form id="step2form"><input type="text" name="numberOfEmployees" id="num-emp" class="" placeholder="# Employee" required><input type="text" name="averageTax" id="avg-tax" class="" placeholder="Average Tax" required><input type="text" name="wage" id="wage" class="" placeholder="Wage" required>'
+var step3FormStr = '<form id="step3form"><input type="text" name="numberOfAutomations" id="num-auto" class="" placeholder="# Automations" required><input type="text" name="cost" id="cost" class="" placeholder="Cost" required><input type="text" name="productivity" id="productivity" class="" placeholder="Productivity" required></form>'
 
 function setKeyUps() {
   $('#employee-add form').last().children().keyup(function() {
@@ -26,8 +30,97 @@ function setKeyUps() {
 }
 
 function setStepTwoKeyUps() {
+  $('#new-emp-add form').last().children().keyup(function() {
+    complete_info = true;
+    $.each($('#new-emp-add form').last().children(), function(index, value) {
+      if ($(value).val().length == 0) {
+        complete_info = false;
+      }
+    })
+    if (complete_info == true) {
 
-}
+      $('#new-emp-add').append(step2FormStr);
+      setStepTwoKeyUps();
 
+      if (stepThree == false) {
+        stepThree == true;
+        $('#third-step h3').fadeIn('slow', function() {
+          $('#automation-add').fadeIn('slow');
+        });
+        setStepThreeKeyUps();
+      }
+    }
+  });
+};
+
+function setStepThreeKeyUps() {
+  $('#automation-add form').last().children().keyup(function() {
+    complete_info = true;
+    $.each($('#automation-add form').last().children(), function(index, value) {
+      if ($(value).val().length == 0) {
+        complete_info = false;
+      }
+    });
+    if (complete_info == true) {
+
+      $('#automation-add').append(step3FormStr);
+      setStepThreeKeyUps();
+
+      if (stepSubmit == false) {
+        stepSubmit == true;
+        $('#submit-step').fadeIn('slow');
+      }
+    }
+  });
+};
 
 setKeyUps();
+
+$("#submit").click(function(e) {
+  e.preventDefault();
+  submitData = {
+    oldEmployees: [],
+    newEmployees: [],
+    automations: []
+  }
+
+  $.each($('#employee-add form'), function(index, value) {
+    var data = JSON.stringify($(value).serializeArray());
+    console.log()
+    if ($($(value).children()[0]).val().length > 0) {
+      submitData['oldEmployees'].push(data);
+    }
+  })
+
+  $.each($('#new-emp-add form'), function(index, value) {
+    var data = JSON.stringify($(value).serializeArray());
+    if ($($(value).children()[0]).val().length > 0) {
+      submitData['newEmployees'].push(data);
+    }
+  })
+
+  $.each($('#automation-add form'), function(index, value) {
+    var data = JSON.stringify($(value).serializeArray());
+    if ($($(value).children()[0]).val().length > 0) {
+      submitData['automations'].push(data);
+    }
+  })
+
+  $.ajax({
+    url: '/calculations',
+    data: submitData,
+    dataType: 'json',
+    type: 'POST',
+    success: function(response) {
+      console.log(response);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+
+  console.log(submitData);
+
+
+  return false;
+});
